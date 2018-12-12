43d3c3a1ee437e7bf2e0c7e0ed7a015b10d524ea
Pawls <43160081+Pawls@users.noreply.github.com>
2018-12-11 19:42:33 -0600

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,13 +1,5 @@
 from Weighted_Graph import *
-#G = Weighted_Graph('test_graph.txt')
-#G.draw_graph()
-#print(G.edge_set())
-#print(G.vertex_set())
-#print(G.edge_dict())
-#H = ({0,1,3}, [(0,1),(1,3)])
-#G.draw_subgraph(H)
 
-#COST of graph e with respect to graph G
 def C(e, G):
     return G.edge_dict()[e]
 
@@ -18,8 +10,7 @@ def incident_edges(T,G):
         for e in G.edge_set():
             if v in e:
                 edges.append(e)
-    return [e for e in edges if e not in T[1]]
-
+    return [e for e in edges if e not in T[1]] 
 
 def valid_incident_edges(T, G):
     edges = incident_edges(T,G)
@@ -43,7 +34,7 @@ def update(T, G):
     T[1].append(e)
     for v in e:
         T[0].add(v)
-    return T     
+    return T
 
 def min_incident_edge(T, G):
     edges = valid_incident_edges(T,G)
